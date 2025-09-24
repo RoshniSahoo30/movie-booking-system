56166084fd46dbf739fcce7683bd16a62cbc4c58
Roshni <roshnisahoo04@gmail.com>
2025-09-24 12:54:02 +0530

    feat: get cinemas details api

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
-const express =require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db"); 
+require("dotenv").config();
+const cinemasRoute = require('./routes/api/cinemas.routes.js');
+
+
 const app = express();
-const port = process.env.PORT||5000;
 
 connectDB();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
-})
+app.use(express.json());
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
+
+//using the routes
+app.use('/api/cinemas', cinemasRoute);
+
+
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
