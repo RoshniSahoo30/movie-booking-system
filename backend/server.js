94b30286e0ab2d5410fb5b53c440284363effd17
Roshni <roshnisahoo04@gmail.com>
2025-09-24 12:33:33 +0530

    mongodb connection implemented

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express =require('express');
+const connectDB = require('./config/db');
+const app = express();
+const port = process.env.PORT||5000;
+
+connectDB();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
