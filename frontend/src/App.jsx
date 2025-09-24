74acfad8923769f209c3964eee82af87fda76d72
Roshni <roshnisahoo04@gmail.com>
2025-09-24 14:43:39 +0530

    setup backend service in frontend and boiler plate code for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
+//importing the pages 
+import HomePage from "./pages/HomePage";
+import CinemaPage from "./pages/CinemaPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
+import BookingHistoryPage from "./pages/BookingHistoryPage";
+import SeatSelectionPage from "./pages/SeatSelectionPage";
 function App() {
-  
-
   return (
-    <>
-      <h1 className="text-5xl">hello</h1>
-    </>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cinema/:cinemaId" element={<CinemaPage />} />
+          <Route path="/show/:showId" element={<SeatSelectionPage />} />
+          <Route path="/confirmation" element={<ConfirmationPage />} />
+          <Route path="/history" element={<BookingHistoryPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+
